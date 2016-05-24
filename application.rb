5649c95c4b14a2f08ec357ff58baa69764bfd391
Michael Fich <michael@michaelfich.com>
2016-05-24 18:36:43 -0400

    Set the minimum required for a Rack application.

--- application.rb
+++ application.rb
@@ -1,3 +1,15 @@
-module Sidewalks
+require 'rack'
 
+module Sidewalks
+  class Application
+    def call(env)
+      status = 200
+      headers = { 'Content-Type' => 'text/html' }
+      body = 'Welcome to Ruby on Sidewalks'
+
+      [ status, headers, [body] ]
+    end
+  end
 end
+
+Rack::Handler::WEBrick.run ::Sidewalks::Application.new
